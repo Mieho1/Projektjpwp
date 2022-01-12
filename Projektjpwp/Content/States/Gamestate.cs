6fc037e4c2d16b07f9de995350b24cf7dd376abd
Mieho1 <m13ch0@gmail.com>
2022-01-12 18:40:15 +0100

    działajacy przycisk menu podczas gry

--- Projektjpwp/Content/States/Gamestate.cs
+++ Projektjpwp/Content/States/Gamestate.cs
@@ -7,6 +7,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Projektjpwp.Backgrounds;
+using Projektjpwp.Content.Controls;
 
 namespace Projektjpwp.Content.States
 {
@@ -14,10 +15,11 @@ namespace Projektjpwp.Content.States
     {
         Texture2D background1;
         Texture2D background2;
+        private List<component> menu;
         SpriteFont font;
         Scrolling scrolling1;
         Scrolling scrolling2;
-        int loop =0;
+        int loop =1;
         string gowno;
 
 
@@ -28,28 +30,45 @@ namespace Projektjpwp.Content.States
         {
             gowno = loop.ToString();
             font = _content.Load<SpriteFont>("Fonts/Title");
+            var buttonTexture = _content.Load<Texture2D>("button/Button");
+            var buttonFont = _content.Load<SpriteFont>("Fonts/bubblefont");
             scrolling1 = new Scrolling(_content.Load<Texture2D>("Background/alternative"),new Rectangle(0,0,1280,1024));
             scrolling2 = new Scrolling(_content.Load<Texture2D>("Background/alternative"), new Rectangle(1280, 0,1280,1024));
-            
-
-
+            var menubutton = new Button(buttonTexture, buttonFont)
+            { 
+                Position= new Vector2(50,50),
+                Text="Menu",
+            };
+            menu = new List<component>()
+      {
+        menubutton
+      };
+            menubutton.Click += menuButton_Click;
 
         }
+
+       
+       
+
+
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
             scrolling1.Draw(spriteBatch);
-         
-
             scrolling2.Draw(spriteBatch);
             spriteBatch.DrawString(font,gowno, new Vector2(200, 200), Color.Black);
+            foreach (var component in menu)
+                component.Draw(gameTime, spriteBatch);
 
             spriteBatch.End();
 
 
         }
-        
+        private void menuButton_Click(object sender, EventArgs e)
+        {
+            _game.ChangeState(new Menustate(_game, _graphicsDevice, _content));
+        }
 
         public override void PostUpdate(GameTime gameTime)
         {
@@ -58,7 +77,8 @@ namespace Projektjpwp.Content.States
 
         public override void Update(GameTime gameTime)
         {   //ruchome tlo 
-
+            foreach (var component in menu)
+                component.Update(gameTime);
             if (scrolling1.rectangle.X + scrolling1.texture.Width <= 0)
             {
                 scrolling1.rectangle.X = scrolling2.rectangle.X + scrolling2.texture.Width;
